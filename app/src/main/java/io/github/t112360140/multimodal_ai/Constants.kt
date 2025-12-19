a91786f869b498edeca381369a0d0a04c8245b47
112360116git <t112360116@ntut.org.tw>
2025-12-19 15:20:05 +0800

    add constant

---
+++ app/src/main/java/io/github/t112360140/multimodal_ai/Constants.kt
@@ -0,0 +1,9 @@
+package io.github.t112360140.multimodal_ai
+
+const val REQUEST_CODE_OK = 0
+const val REQUEST_CODE_UNKNOW_ERROR = 2
+const val REQUEST_CODE_INIT_ERROR = 3
+const val REQUEST_CODE_PERMISSIONS_ERROR = 4
+
+class Constants {
+}
